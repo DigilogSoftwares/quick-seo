facca6e663dc41d0e8a18a8570d00ccb911e9fa3
Mobeen Qamar <mubeenqamar29@gmail.com>
2026-01-09 20:10:33 +0500

    refactor-components

--- app/components/Accordion.jsx
+++ app/components/Accordion.jsx
@@ -1,40 +1,38 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-export const Accordion = () => {
+export const Accordion = ({ items = [] }) => {
   const [expanded, setExpanded] = useState(0);
 
   return (
     <s-section padding="none">
-      {ACCORDION_ITEMS.map(({ title, id, content }) => {
+      {items.map(({ title, id, content }) => {
         const isExpanded = expanded === id;
         return (
-          <s-box
-            key={id}
-            borderWidth="small"
-            borderColor="border"
-          >
+          <s-box key={id} borderWidth="small" borderColor="border">
             <s-clickable
               onClick={() => {
                 setExpanded((prev) => (id === prev ? null : id));
               }}
             >
               <s-box padding="base" background="subdued">
-                <s-stack direction="inline" justifyContent="space-between" alignItems="center">
-                  <s-heading>
-                    {title}
-                  </s-heading>
+                <s-stack
+                  direction="inline"
+                  justifyContent="space-between"
+                  alignItems="center"
+                >
+                  <s-heading>{title}</s-heading>
                   <s-icon type={isExpanded ? "chevron-up" : "chevron-down"} />
                 </s-stack>
               </s-box>
             </s-clickable>
             <div
               style={{
-                display: 'grid',
-                gridTemplateRows: isExpanded ? '1fr' : '0fr',
-                transition: 'grid-template-rows 0.1s ease-out'
+                display: "grid",
+                gridTemplateRows: isExpanded ? "1fr" : "0fr",
+                transition: "grid-template-rows 0.1s ease-out",
               }}
             >
-              <div style={{ overflow: 'hidden' }}>
+              <div style={{ overflow: "hidden" }}>
                 <s-box padding="base" background="bg-surface">
                   {content}
                 </s-box>
@@ -50,46 +48,50 @@ export const Accordion = () => {
 const ACCORDION_ITEMS = [
   {
     id: 0,
-    title: 'Step 1',
+    title: "Step 1",
     content: (
       <s-text>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi tempore, saepe minima
-        nesciunt impedit quaerat repellat eveniet, dignissimos quis quo sed maxime aspernatur qui,
-        quod consectetur optio veritatis iusto eligendi?
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi tempore,
+        saepe minima nesciunt impedit quaerat repellat eveniet, dignissimos quis
+        quo sed maxime aspernatur qui, quod consectetur optio veritatis iusto
+        eligendi?
       </s-text>
-    )
+    ),
   },
   {
     id: 1,
-    title: 'Step 2',
+    title: "Step 2",
     content: (
       <s-text>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi tempore, saepe minima
-        nesciunt impedit quaerat repellat eveniet, dignissimos quis quo sed maxime aspernatur qui,
-        quod consectetur optio veritatis iusto eligendi?
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi tempore,
+        saepe minima nesciunt impedit quaerat repellat eveniet, dignissimos quis
+        quo sed maxime aspernatur qui, quod consectetur optio veritatis iusto
+        eligendi?
       </s-text>
-    )
+    ),
   },
   {
     id: 2,
-    title: 'Step 3',
+    title: "Step 3",
     content: (
       <s-text>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi tempore, saepe minima
-        nesciunt impedit quaerat repellat eveniet, dignissimos quis quo sed maxime aspernatur qui,
-        quod consectetur optio veritatis iusto eligendi?
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi tempore,
+        saepe minima nesciunt impedit quaerat repellat eveniet, dignissimos quis
+        quo sed maxime aspernatur qui, quod consectetur optio veritatis iusto
+        eligendi?
       </s-text>
-    )
+    ),
   },
   {
     id: 3,
-    title: 'Step 4',
+    title: "Step 4",
     content: (
       <s-text>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi tempore, saepe minima
-        nesciunt impedit quaerat repellat eveniet, dignissimos quis quo sed maxime aspernatur qui,
-        quod consectetur optio veritatis iusto eligendi?
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi tempore,
+        saepe minima nesciunt impedit quaerat repellat eveniet, dignissimos quis
+        quo sed maxime aspernatur qui, quod consectetur optio veritatis iusto
+        eligendi?
       </s-text>
-    )
-  }
+    ),
+  },
 ];
